ad12b95e1c02ecd060bea1ca49b28f1b726fd2eb
jin_huang <1309740625@qq.com>
2019-03-14 16:29:27 +0800

    Here are changes made in 2019年3月14日16:30:04

--- src/KITTIdataset.py
+++ src/KITTIdataset.py
@@ -1,13 +1,14 @@
-from torch.utils.data import Dataset, DataLoader
 import numpy as np
-import scipy.io as sio
-from PIL import Image
 import os
+from PIL import Image
+from torch.utils.data import Dataset
 
 
 class KITTIdataset(Dataset):
     """KITTIdataset"""
-    def __init__(self, list_file='train.txt', data_root_path='/home/chaoyang/LKVOLearner/data_kitti', img_size=[128, 416], bundle_size=3):
+
+    def __init__(self, list_file='train.txt', data_root_path='/home/chaoyang/LKVOLearner/data_kitti',
+                 img_size=[128, 416], bundle_size=3):
         self.data_root_path = data_root_path
         self.img_size = img_size
         self.bundle_size = bundle_size
@@ -25,31 +26,28 @@ class KITTIdataset(Dataset):
                     continue
                 frame_path = os.path.join(seq_path, frame_name)
                 self.frame_pathes.append(frame_path)
-                # print(frame_path)
-        # self.frame_pathes = self.frame_pathes[0:40000:800]
 
     def __len__(self):
         return len(self.frame_pathes)
 
     def __getitem__(self, item):
         # read camera intrinsics
-        cam_file = os.path.join(self.data_root_path, self.frame_pathes[item]+'_cam.txt')
+        cam_file = os.path.join(self.data_root_path, self.frame_pathes[item] + '_cam.txt')
         with open(cam_file) as file:
             cam_intrinsics = [float(x) for x in next(file).split(',')]
-        # camparams = dict(fx=cam_intrinsics[0], cx=cam_intrinsics[2],
-        #             fy=cam_intrinsics[4], cy=cam_intrinsics[5])
         camparams = np.asarray(cam_intrinsics)
 
         # read image bundle
-        img_file = os.path.join(self.data_root_path, self.frame_pathes[item]+'.jpg')
+        img_file = os.path.join(self.data_root_path, self.frame_pathes[item] + '.jpg')
         frames_cat = np.array(Image.open(img_file))
         # slice the image into #bundle_size number of images
         frame_list = []
         for i in range(self.bundle_size):
-            frame_list.append(frames_cat[:,i*self.img_size[1]:(i+1)*self.img_size[1],:])
+            frame_list.append(frames_cat[:, i * self.img_size[1]:(i + 1) * self.img_size[1], :])
         frames = np.asarray(frame_list).astype(float).transpose(0, 3, 1, 2)
 
         return frames, camparams
+
 
 if __name__ == "__main__":
     dataset = KITTIdataset()
